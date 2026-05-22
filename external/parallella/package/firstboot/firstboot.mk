3782b8661c5cc9450039a5606766d1bdc4f80259
Nicholas Perry <nick@perrybrandsllc.com>
2026-05-22 22:41:38 +0000

    Improve firstboot and etop package rules

--- external/parallella/package/firstboot/firstboot.mk
+++ external/parallella/package/firstboot/firstboot.mk
@@ -9,6 +9,7 @@ FIRSTBOOT_SITE = $(BR2_EXTERNAL_PARALLELLA_PATH)/../../scripts/firstboot
 FIRSTBOOT_SITE_METHOD = local
 
 define FIRSTBOOT_INSTALL_TARGET_CMDS
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/usr/lib/firstboot
 	$(INSTALL) -D -m 0755 $(@D)/installer.py $(TARGET_DIR)/usr/lib/firstboot/installer.py
 	$(INSTALL) -D -m 0644 $(@D)/parallella-firstboot.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/parallella-firstboot.service
