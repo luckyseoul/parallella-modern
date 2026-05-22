efff47a4089ad81787ef4a1b94d4a9250f428892
Nicholas Perry <nick@perrybrandsllc.com>
2026-05-22 22:25:20 +0000

    Add etop as Buildroot package

---
+++ external/parallella/package/etop/etop.mk
@@ -0,0 +1,13 @@
+ETOP_VERSION = 1.0
+ETOP_SITE = $(TOPDIR)/../scripts/etop
+ETOP_SITE_METHOD = local
+
+define ETOP_BUILD_CMDS
+	$(MAKE) -C $(@D) CC="$(TARGET_CC)"
+endef
+
+define ETOP_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/etop $(TARGET_DIR)/usr/bin/etop
+endef
+
+$(eval $(generic-package))
